d4dbee7041cafc8be6334081c69e8facac20c8cf
oric432 <oric498@gmail.com>
2026-05-05 16:03:50 +0300

    feat: integrate state machine logging into CallSession using boost::sml logger policy

--- src/call/CallSession.cpp
+++ src/call/CallSession.cpp
@@ -7,7 +7,7 @@ namespace SIPI {
 CallSession::CallSession(const InviteReceived& event, boost::asio::io_context& ioc, const Settings& settings)
     : call_id_(event.rdata_->msg_info.cid->id.ptr, static_cast<std::size_t>(event.rdata_->msg_info.cid->id.slen))
     , ctx_(event, ioc, settings)
-    , sm_(ctx_) {
+    , sm_(ctx_, logger_) {
     Log::call()->debug("[{}] CallSession created, about to process event", call_id_);
     sm_.process_event(event);
     Log::call()->debug("[{}] CallSession event processed", call_id_);
--- src/call/CallSession.hpp
+++ src/call/CallSession.hpp
@@ -2,6 +2,7 @@
 
 #include <queue>
 #include <string>
+#include <typeinfo>
 
 #include <boost/asio/io_context.hpp>
 #include <boost/sml.hpp>
@@ -10,8 +11,31 @@
 #include "CallStateMachine.hpp"
 #include "Events.hpp"
 #include "Settings.hpp"
+#include "utils/log.hpp"
 
 namespace SIPI {
+
+struct CallStateLogger {
+    template <class SM, class TEvent>
+    void log_process_event(const TEvent& /*event*/) {
+        // Log::call()->debug("[sml] event {}", typeid(TEvent).name());
+    }
+
+    template <class SM, class TGuard, class TEvent>
+    void log_guard(const TGuard& /*guard*/, const TEvent& /*event*/, bool /*result*/) {
+        // Log::call()->debug("[sml] guard {} -> {}", typeid(TGuard).name(), result);
+    }
+
+    template <class SM, class TAction, class TEvent>
+    void log_action(const TAction& /*action*/, const TEvent& /*event*/) {
+        // Log::call()->debug("[sml] action {}", typeid(TAction).name());
+    }
+
+    template <class SM, class TSrcState, class TDstState>
+    void log_state_change(const TSrcState& src, const TDstState& dst) {
+        Log::call()->debug("[sml] state {} -> {}", src.c_str(), dst.c_str());
+    }
+};
 
 class CallSession {
 public:
@@ -33,7 +57,10 @@ public:
 private:
     std::string call_id_;
     CallContext ctx_;
-    boost::sml::sm<CallStateMachine<CallContext>, boost::sml::process_queue<std::queue>> sm_;
+    CallStateLogger logger_;
+    boost::sml::
+        sm<CallStateMachine<CallContext>, boost::sml::process_queue<std::queue>, boost::sml::logger<CallStateLogger>>
+            sm_;
 };
 
 } // namespace SIPI
